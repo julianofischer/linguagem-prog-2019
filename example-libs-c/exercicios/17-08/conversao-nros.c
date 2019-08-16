a2890da74bac1c8cd727015ab8bacb0555db5c4f
Juliano Fischer Naves <julianofischer@gmail.com>
2019-08-16 14:25:43 -0400

    adicionado template para exercicios do dia 17-8

---
+++ example-libs-c/exercicios/17-08/conversao-nros.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include<string.h>
+
+/*  17/08/2019 
+ *  Faça um programa que leia um inteiro n e posteriormente
+ *  leia n números em formato de string, converta-os e 
+ *  exiba-os.
+*/
+int main() {
+    return 0;
+}
---
+++ example-libs-c/exercicios/17-08/numeros-aleatorios.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include<string.h>
+
+/*  17/08/2019 
+ *  Faça um programa que leia um inteiro n e posteriormente
+ *  gere e exiba n números aleatórios. Posteriormente leia 
+ *  um inteiro s, utilize-o como semente e gere e exiba 
+ *  n números aleatórios novamente.
+*/
+int main() {
+    return 0;
+}
